0e274d997a139ffbe2ecffd80e92f1ddf6a6aa2e
carlosdam <charlygm2004@gmail.com>
2024-11-06 16:04:51 +0100

    Continuando el código

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import tkinter as tk
 
+#Menu principal
 def crearMenu(dimensiones):
     main = tk.Tk()
     main.geometry(dimensiones)
@@ -13,5 +14,11 @@ def crearMenu(dimensiones):
 def crearBotones(main):
     button = tk.Button(main, text="Nueva contraseña", command=NotImplemented)
     button.pack()
+    
+#Menú para insertar una contraseña y su dominio al que pertenece.
+def menuNuevaContrasenya():
+    return NotImplemented
+
 if __name__ == "__main__":
-    crearMenu("300x300")
+    crearMenu("500x300")
+
---
+++ model/hash.py
@@ -0,0 +1,9 @@
+#Clase para guardar los hash y aplicar la encriptacion
+'''
+HACER UN MÉTODO PARA GUARDAR EN TXT
+'''
+class Hash:
+    def __init__(self, hashcode):
+        self.hashcode = hashcode
+
+
